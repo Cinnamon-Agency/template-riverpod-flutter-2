73907a82f0e1a6838fcc8df1b74641939d974385
LRadocaj <leo.radocaj@cinnamon.agency>
2023-09-29 11:27:27 +0200

    setup service

--- lib/infra/local_notifications/entity/notification.dart
+++ lib/infra/local_notifications/entity/notification.dart
@@ -1,13 +1,20 @@
-class AppNotification {
+import 'package:equatable/equatable.dart';
+import 'package:timezone/timezone.dart';
+
+class AppNotification extends Equatable {
   final String id;
   final String title;
   final String description;
-  final Map<String,dynamic> payload;
-
+  final Map<String, dynamic> payload;
+  final TZDateTime scheduledDate;
   const AppNotification({
     required this.id,
     required this.title,
     required this.description,
     required this.payload,
+    required this.scheduledDate,
   });
+
+  @override
+  List<Object?> get props => [id, title, description, payload, scheduledDate];
 }
