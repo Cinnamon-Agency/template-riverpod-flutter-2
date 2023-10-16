3fd0b209eb41aae96be65275493e7732bf486e34
josipbarisic <josip.barisic1998@gmail.com>
2023-10-16 10:06:52 +0200

    Update: Finish log out logic

--- lib/features/profile/controller/profile_controller.dart
+++ lib/features/profile/controller/profile_controller.dart
@@ -1,25 +1,26 @@
-import 'dart:developer';
-
 import 'package:cinnamon_riverpod_2/features/profile/controller/profile_state.dart';
+import 'package:cinnamon_riverpod_2/infra/auth/service/auth_service.dart';
+import 'package:cinnamon_riverpod_2/infra/auth/service/firebase_auth_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final AutoDisposeStateNotifierProvider<ProfileController, ProfileState>
     profileControllerProvider =
     StateNotifierProvider.autoDispose<ProfileController, ProfileState>(
   (AutoDisposeStateNotifierProviderRef<ProfileController, ProfileState> ref) =>
-      ProfileController(),
+      ProfileController(ref.read(authServiceProvider)),
 );
 
 class ProfileController extends StateNotifier<ProfileState> {
-  ProfileController() : super(const ProfileState());
+  ProfileController(this.firebaseAuthService) : super(const ProfileState());
+
+  FirebaseAuthService firebaseAuthService;
 
   Future<bool> logOut() async {
     state = const ProfileState(loading: true);
-    log('Logging out');
-    // TODO(Josip): Implement log out logic
-    await Future<void>.delayed(const Duration(seconds: 3));
+
+    await firebaseAuthService.logout();
+
     state = const ProfileState(loading: false);
-    log('Logged out');
-    return true;
+    return firebaseAuthService.auth.currentUser == null;
   }
 }
