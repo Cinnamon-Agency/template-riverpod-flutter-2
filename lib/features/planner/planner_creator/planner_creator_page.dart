8c341dcf6b0e43ed1822ef10965e0bd8b4e75d48
Tea-D <tea.devcic@cinnamon.agency>
2023-10-30 11:31:50 +0100

    Add: Planner creator form

--- lib/features/planner/planner_creator/planner_creator_page.dart
+++ lib/features/planner/planner_creator/planner_creator_page.dart
@@ -1,10 +1,15 @@
+import 'package:cinnamon_riverpod_2/features/planner/planner_creator/widgets/planner_creator_form.dart';
 import 'package:flutter/material.dart';
 
-class BenchPage extends StatelessWidget {
-  const BenchPage({Key? key}) : super(key: key);
+class PlannerCreatorPage extends StatelessWidget {
+  const PlannerCreatorPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Placeholder();
-  }
+  Widget build(BuildContext context) => const Padding(
+    padding: EdgeInsets.all(20.0),
+    child: Column(children: [
+          //  Container(color: Colors.red, height: 100,),
+          Expanded(child: PlannerCreatorForm()),
+        ]),
+  );
 }
